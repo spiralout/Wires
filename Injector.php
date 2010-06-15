385444e3ef50cc6ea2c314f5ba310674c362d342
Sean Crystal <seancrystal@gmail.com>
2010-06-15 08:11:33 -0500

    Fixing method call

--- Injector.php
+++ Injector.php
@@ -16,11 +16,11 @@ class Wires_Injector implements Wires_Injector_Interface
 	 * @param Wires_Locator_Interface $locator
 	 */
 	function __construct(Wires_Locator_Interface $locator)
-   {
+	{
 		$this->locator = $locator;
 	}
 
-   /**
+	/**
 	 * Get an instance of $class with dependencies injected
 	 *
 	 * @param string $class
@@ -28,7 +28,7 @@ class Wires_Injector implements Wires_Injector_Interface
 	 * @return mixed
 	 */
 	function create($class, $context = Wires_Locator_Interface::GLOBAL_CONTEXT)
-   {
+	{
 		if (!($obj = $this->getBinding($class, $context)))	{
 			throw new Wires_Exception_NotBound($class, $context);
 		} elseif (is_object($obj)) {
@@ -37,33 +37,33 @@ class Wires_Injector implements Wires_Injector_Interface
 
 		$ref_class = new ReflectionClass($obj);
 
-      // if class has a constructor, fill in args
+		// if class has a constructor, fill in args
 		if ($ref_method = $ref_class->getConstructor())	{
 			return $ref_class->newInstanceArgs($this->getConstructorArgs($ref_method->getParameters(), $ref_class));
 
-      // if class is not instantiable, get a bound class that is
+  		// if class is not instantiable, get a bound class that is
 		} elseif (!$ref_class->isInstantiable()) {
-			return $this->getInstance($ref_class->getName(), $context);
+			return $this->create($ref_class->getName(), $context);
 
-      // class is instantiable and has no constructor, just instantiate
+  		// class is instantiable and has no constructor, just instantiate
 		} else {
 			return $ref_class->newInstance();
 		}
 	}
 
-   /**
-    * Create a new injector with additional bindings (overriding previously defined ones)
-    *
-    * @param  $bindings array
-    * @return Wires_Injector
-    */
-   function with(array $bindings)
-   {
-      $locator = clone($this->locator);
-      $locator->loadBindings($bindings, true);
+	/**
+	 * Create a new injector with additional bindings (overriding previously defined ones)
+	 *
+	 * @param  $bindings array
+	 * @return Wires_Injector
+	 */
+	function with(array $bindings)
+	{
+		$locator = clone($this->locator);
+		$locator->loadBindings($bindings, true);
 
-      return new Wires_Injector($locator);
-   }
+		return new Wires_Injector($locator);
+	}
 
 	/**
 	 * Get a hierarchy of contexts for the given class as an array
@@ -72,7 +72,7 @@ class Wires_Injector implements Wires_Injector_Interface
 	 * @return array
 	 */
 	private function getContexts($class)
-   {
+	{
 		if ($class == Wires_Locator_Interface::GLOBAL_CONTEXT) {	
 			return array($class);
 		}
@@ -80,14 +80,14 @@ class Wires_Injector implements Wires_Injector_Interface
 		$ref = new ReflectionClass($class);
 		$contexts = array($class);
 
-      // collect interfaces
+		// collect interfaces
 		if (is_array($ref->getInterfaceNames())) {	
 			foreach ($ref->getInterfaceNames() as $interface) {	
 				$contexts[] = $interface;
 			}
 		}
 
-      // add parent class
+		// add parent class
 		if ($ref->getParentClass()) {
 			$parent_contexts = $this->getContexts($ref->getParentClass()->getName());
 
@@ -112,25 +112,25 @@ class Wires_Injector implements Wires_Injector_Interface
 	 * @return mixed
 	 */
 	private function getBinding($class, $context = Wires_Locator_Interface::GLOBAL_CONTEXT)
-   {
+	{
 		$ref = new ReflectionClass($class);
 		$contexts = $this->getContexts($context);
 
 		foreach ($contexts as $context) {	
-			
+
 			// first check for bound singleton
 			if ($this->locator->boundSingleton($class, $context))	{
-				
+
 				// if singleton already instantiated grab ref to it
 				if ($this->locator->boundInstance($class, $context)) {
 					$obj = $this->locator->getInstance($class, $context);
-					
-				// otherwise instantiate singleton					
+
+					// otherwise instantiate singleton					
 				} else {
 					$obj = $this->create($this->locator->getBinding($class, $context), $context);
 					$this->locator->bindInstance($class, $obj, $context);
 				}
-				
+
 				return $obj;
 			}
 
@@ -140,7 +140,7 @@ class Wires_Injector implements Wires_Injector_Interface
 			}
 		}
 
-      // if $class is itself instantiable, return that
+		// if $class is itself instantiable, return that
 		if ($ref->isInstantiable()) {	
 			return $class;
 		}
@@ -156,10 +156,10 @@ class Wires_Injector implements Wires_Injector_Interface
 	 * @return mixed
 	 */
 	private function getBoundValue($name, $context = Wires_Locator_Interface::GLOBAL_CONTEXT)
-   {
+	{
 		$contexts = $this->getContexts($context);
 
-      // search contexts until we find a value
+		// search contexts until we find a value
 		foreach ($contexts as $context) {	
 			if ($this->locator->boundValue($name, $context)) {	
 				return $this->locator->getValue($name, $context);
@@ -176,57 +176,57 @@ class Wires_Injector implements Wires_Injector_Interface
 	 * @param string $context
 	 * @return mixed
 	 */
-   private function getBoundClass($name, $context = Wires_Locator_Interface::GLOBAL_CONTEXT)
-   {
-      $contexts = $this->getContexts($context);
+	private function getBoundClass($name, $context = Wires_Locator_Interface::GLOBAL_CONTEXT)
+	{
+		$contexts = $this->getContexts($context);
 
-      // search contexts until we find a class name
-      foreach ($contexts as $context) {
-         if ($this->locator->boundClass($name, $context)) {
-            return $this->locator->getClass($name, $context);
-         }
-      }
+		// search contexts until we find a class name
+		foreach ($contexts as $context) {
+			if ($this->locator->boundClass($name, $context)) {
+				return $this->locator->getClass($name, $context);
+			}
+		}
 
-      return false;
-   }
+		return false;
+	}
 
-   /**
-    * Get a value for each constructor param for $ref_class
-    *
-    * @param array $params
-    * @param string $ref_class 
-    * @return array
-    */
-   private function getConstructorArgs($params, $ref_class)
-   {
-      $args = array();
+	/**
+	 * Get a value for each constructor param for $ref_class
+	 *
+	 * @param array $params
+	 * @param string $ref_class 
+	 * @return array
+	 */
+	private function getConstructorArgs($params, $ref_class)
+	{
+		$args = array();
 
-      foreach ($params as $i => $param) {
-         $param_class = ($param->getClass() ? $param->getClass()->getName() : null);
+		foreach ($params as $i => $param) {
+			$param_class = ($param->getClass() ? $param->getClass()->getName() : null);
 
-         // param has class type hint
-         if ($param_class)	{
-            $args[] = $this->create($param_class, $ref_class->getName());
+			// param has class type hint
+			if ($param_class)	{
+				$args[] = $this->create($param_class, $ref_class->getName());
 
-         // class bound to param name
-         } elseif ($param_class = $this->getBoundClass($param->getName(), $ref_class->getName())) {
-            $args[] = $this->create($param_class, $ref_class->getName());
+				// class bound to param name
+			} elseif ($param_class = $this->getBoundClass($param->getName(), $ref_class->getName())) {
+				$args[] = $this->create($param_class, $ref_class->getName());
 
-         // value bound to param name
-         } elseif ($this->getBoundValue($param->getName(), $ref_class->getName())) {
-            $args[] = $this->getBoundValue($param->getName(), $ref_class->getName());
+				// value bound to param name
+			} elseif ($this->getBoundValue($param->getName(), $ref_class->getName())) {
+				$args[] = $this->getBoundValue($param->getName(), $ref_class->getName());
 
-         // couldn't figure it out, set param to null
-         // @TODO: warning or exception here?
-         } else {
-            $args[] = null;
-         }
-      }
+				// couldn't figure it out, set param to null
+				// @TODO: warning or exception here?
+			} else {
+				$args[] = null;
+			}
+		}
 
-      return $args;
-   }
+		return $args;
+	}
 
-   
+
 	/** @var Wires_Locator_Interface */
 	private $locator;
 }
